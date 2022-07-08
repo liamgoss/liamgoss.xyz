80ca8cc0690cfe8a3c34a70576bda862c1122c77
Liam Goss <50380089+liamgoss@users.noreply.github.com>
2022-07-07 22:03:01 -0700

    Added more projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,15 +4,18 @@ import arbitrade from '../assets/Arbitrade.png';
 import usnscc from '../assets/USNSCC.png';
 import Modal from './Modal/Modal';
 import usb2can from '../assets/USB2CAN_top.jpg';
+import ethCircuit from '../assets/Ethereum_Circuits.jpg';
+import tfLogo from "../assets/Tensorflow_logo.png";
+import ipfsLogo from "../assets/ipfs.png";
 
 const Projects = () => {
     const [height, setHeight] = useState(0)
     const ref = useRef(null)
-
     useEffect(() => {setHeight(ref.current.clientHeight)})
 
-    var items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
     return (
+        <>
         <section className="projects">
             <h2>My Projects</h2>
 
@@ -25,7 +28,7 @@ const Projects = () => {
 
                     
                     <img src={arbitrade} alt="Arbitrade" />
-                    <p>A decentralized cryptocurrency trading platform designed for risk-free arbitrage trading using flashloans!</p>
+                    <center>A decentralized cryptocurrency trading platform designed for risk-free arbitrage trading using flashloans!</center>
 
                     <div style={{
                         display: 'flex',
@@ -41,8 +44,8 @@ const Projects = () => {
                         <h3>Nissan CAN Bus Reverse Engineering</h3>
                     </div>
                     <img src={usb2can} alt="Nissan CAN Bus Reverse Engineering" />
-                    <p>Python code written to utilize the Korlan usb2can hardware to send and receive data over the can-bus on a 2008 Nissan 350z
-                    </p>
+                    <center>Python code written to utilize the Korlan usb2can hardware to send and receive data over the can-bus on a 2008 Nissan 350z
+                    </center>
                     
                     <a href="https://github.com/liamgoss/nissan_ecu_hacking" target="_blank" className="button" style={{
                         display: 'flex',
@@ -64,13 +67,76 @@ const Projects = () => {
                     </div>
                     
                     <img src={usnscc} alt="Nonprofit Web Development" />
-                    <p>Built and maintained the website for the United States Naval Sea Cadet Corps Kings County Squadron</p>
-
+                    <center>Built and maintained the website for the United States Naval Sea Cadet Corps Kings County Squadron</center>
+                    <br></br>
                     
                 </div>
+
+                <div className="projects__card" style={{ height: 'auto' }} >
+                    
+
+                    <div style={{display: 'flex',
+                        alignItems: 'center',
+                        textAlign: 'center'}}><h3>Ethereum Wallet Private Key Generator</h3>
+                    </div>
+                    
+                    <img src={ethCircuit} alt="Ethereum circuit logo" />
+                    <center>This code is also useful for creating your own Ethereum mainnet compatible address and private key without the need for a third party like Metamask! This code is for educational purposes only</center>
+                    <a href="https://github.com/liamgoss/Ethereum-Wallet-Private-Key-Generation" target="_blank" className="button" style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        
+                    }}>Code</a> 
+                    
+                </div>
+
+                <div className="projects__card" style={{ height: 'auto' }} >
+                    
+
+                    <div style={{display: 'flex',
+                        alignItems: 'center',
+                        textAlign: 'center'}}><h3>Neural Networks and Machine Learning</h3>
+                    </div>
+                    
+                    <img src={tfLogo} alt="Tensorflow logo" />
+                    <center>A random compilation of Machine Learning and Neural Networks I have worked on (most will include Tensorflow, Keras, etc.)
+
+Notable projects present here: Flappy Bird AI, SMS Spam Detection, and ReCaptcha V2 image identificaiton (WIP), art producing GAN (WIP)</center>
+                    <a href="https://github.com/liamgoss/neuralNetworks" target="_blank" className="button" style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        
+                    }}>Code</a> 
+                    
+                </div>
+
+
+                <div className="projects__card" style={{ height: 'auto' }} >
+                    
+
+                    <div style={{display: 'flex',
+                        alignItems: 'center',
+                        textAlign: 'center'}}><h3>This website, liamgoss.xyz, is hosted using IPFS</h3>
+                    </div>
+                    
+                    <img src={ipfsLogo} alt="IPFS logo" />
+                    <center>This website was made using React.js in combination with Fleek.co to completely host and store this site using IPFS!</center>
+                    <a href="https://github.com/liamgoss/neuralNetworks" target="_blank" className="button" style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        
+                    }}>Code</a> 
+                    
+                </div>
+
             </div>
+
             
         </section>
+        </>
     );
 }
 
--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
             
-            <a href="mailto:liamjgoss@gmail.com" className="button">liamjgoss@gmail.com</a>
-            <a href="https://twitter.com/liamjgoss" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/in/liamgoss/" className="button">LinkedIn</a>
-            <a href="https://github.com/liamgoss" className="button">GitHub</a>
+            <a href="mailto:liamjgoss@gmail.com" className="buttonSocial">liamjgoss@gmail.com</a>
+            <a href="https://twitter.com/liamjgoss" className="buttonSocial">Twitter</a>
+            <a href="https://www.linkedin.com/in/liamgoss/" className="buttonSocial">LinkedIn</a>
+            <a href="https://github.com/liamgoss" className="buttonSocial">GitHub</a>
         </div>
     );
 }
